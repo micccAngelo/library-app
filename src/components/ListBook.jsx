2fb2e28afef040280437ec9fd5db71cfdf698da1
micccAngelo <michaelangelo0109@gmail.com>
2023-03-02 12:08:37 +0700

    loading_button

--- src/components/ListBook.jsx
+++ src/components/ListBook.jsx
@@ -13,7 +13,6 @@ function ListBook() {
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState(0);
   const [loading, setLoading] = useState(false);
-  const [cartLoading, setCartLoading] = useState(false);
   const navigate = useNavigate();
 
   useEffect(() => {
